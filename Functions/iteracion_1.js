408ab345a516c521096835cb3e9caf6ee4e91f73
Pedro Gonzalez <pedrogonzalezfernandez@gmail.com>
2022-03-01 14:20:02 +0100

    functions 1 y 2

---
+++ Functions/iteracion_1.js
@@ -0,0 +1,9 @@
+/*
+**Iteración #1: Buscar el máximo**
+
+Completa la función que tomando dos números como argumento devuelva el más alto.
+*/
+
+function max(numberOne, numberTwo) {
+  return numberOne > numberTwo ? numberOne : numberTwo;
+}
---
+++ Functions/iteracion_2.js
@@ -0,0 +1,25 @@
+/*
+**Iteración #2: Buscar la palabra más larga**
+
+Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+Puedes usar este array para probar tu función:
+*/
+
+const avengers = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(list) {
+  let longestWord = "";
+  list.forEach((item) => {
+    if (item.length > longestWord.length) longestWord = item;
+  });
+  return longestWord;
+}
+
+console.log(findLongestWord(avengers));
